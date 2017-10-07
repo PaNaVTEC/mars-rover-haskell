cb97fc530c6e611aaaab5e8d99672c078ffd4389
Christian Panadero <panavtec@gmail.com>
2017-10-07 22:22:10 +0100

    pattern match over direction only

--- src/Me/Panavtec/Mars/Marsrover.hs
+++ src/Me/Panavtec/Mars/Marsrover.hs
@@ -4,6 +4,6 @@ module Me.Panavtec.Mars.Marsrover (
 
 moveMars :: String -> String
 moveMars orders = next orders "0,0,N"
-  where next [] currentPosition           = currentPosition
-        next ('L' : restOfOrders) "0,0,N" =  next restOfOrders "0,0,W"
-        next ('L' : restOfOrders) "0,0,W" =  next restOfOrders "0,0,S"
+  where next [] currentPosition     = currentPosition
+        next ('L' : orders) (x : ',' : y : ',' : 'N' : _) =  next orders "0,0,W"
+        next ('L' : orders) (x : ',' : y : ',' : 'W' : _)=  next orders "0,0,S"
