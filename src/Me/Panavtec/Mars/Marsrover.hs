48bcec2bdc4cc6e992388929971fa97cb118e096
Christian Panadero <panavtec@gmail.com>
2017-10-08 12:47:45 +0100

    Have you seen that duplication? Me too, let me start removing it

--- src/Me/Panavtec/Mars/Marsrover.hs
+++ src/Me/Panavtec/Mars/Marsrover.hs
@@ -11,15 +11,10 @@ rightDirection = cycle(['N', 'E', 'S', 'W'])
 moveMars :: String -> String
 moveMars orders = next orders "0,0,N"
   where next [] currentPosition      = currentPosition
-        next ('L' : orders) position = next orders (rotateLeft position)
-        next ('R' : orders) position = next orders (rotateRight position)
+        next ('L' : orders) position = next orders (rotate leftDirection position)
+        next ('R' : orders) position = next orders (rotate rightDirection position)
 
-rotateRight :: String -> String
-rotateRight (x:',':y:',':dir:_) = x:',':y:',': (next dir) :[]
-  where next dir = rightDirection !! nextIndex
-        nextIndex = fromJust(elemIndex dir rightDirection) + 1
-
-rotateLeft :: String -> String
-rotateLeft (x:',':y:',':dir:_) = x:',':y:',': (next dir) :[]
-  where next dir = leftDirection !! nextIndex
-        nextIndex = fromJust(elemIndex dir leftDirection) + 1
+rotate :: [Char] -> String -> String
+rotate directions (x:',':y:',':dir:_) = x:',':y:',': (next dir) :[]
+  where next dir = directions !! nextIndex
+        nextIndex = fromJust(elemIndex dir directions) + 1
