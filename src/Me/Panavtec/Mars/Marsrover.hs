c89c13319a2170d0e021d9b3ad02456189f218b5
Christian Panadero <panavtec@gmail.com>
2017-10-08 14:20:11 +0100

    Extract knowledge from execute to rotate

--- src/Me/Panavtec/Mars/Marsrover.hs
+++ src/Me/Panavtec/Mars/Marsrover.hs
@@ -9,12 +9,11 @@ leftDirection = cycle(['N', 'W', 'S', 'E'])
 rightDirection = cycle(['N', 'E', 'S', 'W'])
 
 moveMars :: String -> String
-moveMars orders = foldl (\b a -> rotateTo a b) "0,0,N" orders
-  where rotateTo 'L' currentPos = rotate leftDirection currentPos
-        rotateTo 'R' currentPos = rotate rightDirection currentPos
+moveMars orders = foldl (\b a -> rotate a b) "0,0,N" orders
 
-rotate :: [Char] -> String -> String
-rotate directions (x:',':y:',':currentDirection:_) = x:',':y:',': nextDirection :[]
-  where nextDirection = directions !! nextIndex
+rotate :: Char -> String -> String
+rotate direction (x:',':y:',':currentDirection:_) = x:',':y:',': nextDirection :[]
+  where directions = if direction == 'L' then leftDirection else rightDirection
+        nextDirection = directions !! nextIndex
         currentIndex = elemIndex currentDirection directions
         nextIndex = fromJust(currentIndex) + 1
