4e11412d292c1c2cebf8024b643118269f56c1c3
Christian Panadero <panavtec@gmail.com>
2017-10-08 19:52:48 +0100

    extract createPosition function

--- src/Me/Panavtec/Mars/Marsrover.hs
+++ src/Me/Panavtec/Mars/Marsrover.hs
@@ -13,12 +13,15 @@ initialPosition = "0,0,N"
 moveMars :: String -> String
 moveMars orders = foldl (\position order -> if order == 'M' then move position else rotate order position) initialPosition orders
 
+createPosition :: Char -> Char -> Char -> String
+createPosition x y d = x : ',' : y : ',' : d : []
+
 move :: String -> String
 move (x:',':y:',':direction:_)
-  | direction == 'E' = moveEast:',':y:',':'E':[]
-  | direction == 'N' = x:',':moveNorth:',':'N':[]
-  | direction == 'W' = moveWest:',':y:',':'W':[]
-  | direction == 'S' = x:',':moveSouth:',':'S':[]
+  | direction == 'E' = createPosition moveEast y direction
+  | direction == 'N' = createPosition x moveNorth direction
+  | direction == 'W' = createPosition moveWest y direction
+  | direction == 'S' = createPosition x moveSouth direction
   where
     bounds = cycle([0..9])
     incrementBound bound = intToDigit $ bounds !! (digitToInt bound + 1)
@@ -30,7 +33,7 @@ move (x:',':y:',':direction:_)
     moveWest = decrementBound x
 
 rotate :: Char -> String -> String
-rotate direction (x:',':y:',':currentDirection:_) = x:',':y:',': nextDirection :[]
+rotate direction (x:',':y:',':currentDirection:_) = createPosition x y nextDirection
   where directions = if direction == 'L' then leftDirection else rightDirection
         nextDirection = directions !! nextIndex
         currentIndex = elemIndex currentDirection directions
