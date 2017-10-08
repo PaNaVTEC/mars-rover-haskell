8aa0c69736bc928f60bbd9b5b56634be4d3368a3
Christian Panadero <panavtec@gmail.com>
2017-10-08 14:17:34 +0100

    Don't use recursion, just folding

--- src/Me/Panavtec/Mars/Marsrover.hs
+++ src/Me/Panavtec/Mars/Marsrover.hs
@@ -9,10 +9,9 @@ leftDirection = cycle(['N', 'W', 'S', 'E'])
 rightDirection = cycle(['N', 'E', 'S', 'W'])
 
 moveMars :: String -> String
-moveMars orders = next orders "0,0,N"
-  where next [] currentPosition      = currentPosition
-        next ('L' : orders) position = next orders (rotate leftDirection position)
-        next ('R' : orders) position = next orders (rotate rightDirection position)
+moveMars orders = foldl (\b a -> rotateTo a b) "0,0,N" orders
+  where rotateTo 'L' currentPos = rotate leftDirection currentPos
+        rotateTo 'R' currentPos = rotate rightDirection currentPos
 
 rotate :: [Char] -> String -> String
 rotate directions (x:',':y:',':currentDirection:_) = x:',':y:',': nextDirection :[]
